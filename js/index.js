99d06550e64d9aa9854e485c285b0a84ab4d9342
Ana Harris <vana1990@gmail.com>
2018-11-24 15:57:30 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,30 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const main = document.getElementById('main');
+const input = document.querySelector('input');
+let divs = document.querySelectorAll('div');
+
+main.addEventListener('click', function(event){
+  alert('I was clicked!');
+});
+
+
+input.addEventListener('keydown', function(e){
+  console.log(e.which);
+});
+
+input.addEventListener('keydown' function(e){
+  if (e.which === 71) {
+    return e.preventDefault()
+  } else {
+    console.log(e.which);
+  }
+});
+
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i=0, i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
