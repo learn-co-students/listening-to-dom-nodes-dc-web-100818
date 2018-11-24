41363e2d1c4a759d85acf3f0dd4e5fb6d8b6a295
Drewisatlas <drewisatlas@gmail.com>
2018-11-24 18:17:19 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+const main = document.getElementById('main');
+const input = document.querySelector('input');
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+
+input.addEventListener('keydown', function(e){
+  console.log(e.which);
+})
